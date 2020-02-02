8bb165047ca9d073649600d6e2bb156d83ebaff2
Sergio Nizama <sergio.nizama@outlook.com>
2020-02-01 21:22:05 -0500

    added react-swipeable and implemented logic into GuestList and Schedule

--- src/routes/GuestList.tsx
+++ src/routes/GuestList.tsx
@@ -1,32 +1,32 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
-import guestList from "../assets/guests.json";
-import Card from "../components/Card";
-import Toggle from "../components/Toggle";
-import useBookmarks from "../hooks/useBookmarks";
-import firebaseApp from "../util/firebaseApp";
-import { Transition } from "react-transition-group";
+import guestList from '../assets/guests.json';
+import Card from '../components/Card';
+import Toggle from '../components/Toggle';
+import useBookmarks from '../hooks/useBookmarks';
+import firebaseApp from '../util/firebaseApp';
 
-const transitionStyles = {
-  entering: { opacity: 1, transform: "scale(1.1)" },
-  entered: {
-    opacity: 1,
-    transform: "scale(1)",
-    transition: "opacity 400ms, transform 400ms"
-  },
-  exiting: { opacity: 1, transform: "scale(1)" },
-  exited: {
-    opacity: 0,
-    transform: "scale(0.9)",
-    transition: "opacity 400ms, transform 400ms"
-  }
-};
+import SwipeableViews from 'react-swipeable-views';
+import classed from 'classed-components';
+
+const GuestButton = classed.button<{ current?: boolean }>`
+  w-2/5
+  border
+  rounded-lg
+  border-green-600
+  h-8
+  focus:outline-none
+  m-1
+  font-bold
+  ${({ current }) =>
+    current ? 'bg-green-600 text-white' : 'bg-white text-green-700'}
+`;
 
 const GuestList: React.FC = () => {
-  // const { day_one: saturday, day_two: sunday } = eventSchedule.schedule_2020;
   const [showBookmarks, setShowBookmarks] = React.useState(false);
+  const [index, setIndex] = React.useState(0);
   let { guests_2020: guests } = guestList;
 
   const [user] = useAuthState(firebaseApp.auth());
@@ -40,70 +40,122 @@ const GuestList: React.FC = () => {
     setShowBookmarks(!showBookmarks);
   };
 
+  const handleIndexChange = (index: number, lastIndex: number) => {
+    setIndex(index);
+  };
+
+  const handleGuestChange = (index: number) => () => {
+    setIndex(index);
+  };
+
   if (showBookmarks) {
     guests = guests.filter(guest => bookmarks?.includes(guest.slug));
   }
 
+  const talets = guests.filter(guest => guest.category === 'Talent');
+  const exhibitors = guests.filter(guest => guest.category === 'Exhibitor');
+  const artists = guests.filter(guest => guest.category === 'Artist');
+  const writers = guests.filter(guest => guest.category === 'Writer');
+
   return (
-    <div>
-      {bookmarks && (
+    <div className="h-screen">
+      <div className="flex items-center justify-between w-full mt-1 mb-2">
+        <div className="flex flex-wrap justify-around flex-grow">
+          <GuestButton current={index === 0} onClick={handleGuestChange(0)}>
+            Talents
+          </GuestButton>
+          <GuestButton current={index === 1} onClick={handleGuestChange(1)}>
+            Exhibitors
+          </GuestButton>
+          <GuestButton current={index === 2} onClick={handleGuestChange(2)}>
+            Artists
+          </GuestButton>
+          <GuestButton current={index === 3} onClick={handleGuestChange(3)}>
+            Writers
+          </GuestButton>
+        </div>
+        {bookmarks && <Toggle onChange={handleCheckbox} />}
+      </div>
+      {/* {bookmarks && (
         <div className="flex justify-end w-full my-2">
           <Toggle onChange={handleCheckbox}>Show bookmarks</Toggle>
         </div>
-      )}
-      <ul>
-        {guests.map((guest, i) => {
-          const isBookmarked = bookmarks?.includes(guest.slug);
-          return (
-            <li key={i}>
-              <Card className="my-5">
-                <button
-                  className="w-full focus:outline-none"
-                  onClick={handleClick(guest.website.link)}
-                >
-                  <img
-                    className="w-full rounded"
-                    src={guest.img}
-                    alt={guest.slug}
-                  />
-                </button>
-                <div className="flex items-center justify-between">
-                  <button
-                    className="w-10/12 px-4 py-2 focus:outline-none"
-                    onClick={handleClick(guest.website.link)}
-                  >
-                    <div className="text-2xl text-left">{guest.name}</div>
-                    <div className="text-sm text-left">
-                      {guest.description}
-                    </div>
-                  </button>
-                  <div className="w-2/12 pr-3">
-                    <button
-                      className={`w-12 h-12 p-1 border-2 ${
-                        isBookmarked
-                          ? "bg-green-500 border-green-600"
-                          : "bg-white border-gray-400"
-                      } focus:outline-none block mx-auto shadow-md`}
-                      onClick={() => handleBookmark(guest.slug)}
-                      style={{ borderRadius: 50 }}
-                    >
-                      <FontAwesomeIcon
-                        className={`text-2xl ${
-                          isBookmarked ? "text-white" : "text-green-400"
-                        } my-2`}
-                        icon="heart"
-                      />
-                    </button>
-                  </div>
-                </div>
-              </Card>
-              {/* </button> */}
-            </li>
-          );
-        })}
-      </ul>
+      )} */}
+      <SwipeableViews
+        containerStyle={{
+          height: '100vh',
+          WebkitOverflowScrolling: 'touch',
+        }} /* index={index} onChangeIndex={handleIndexChange} */
+        index={index}
+        onChangeIndex={handleIndexChange}
+      >
+        <ul className="m-2">
+          {talets.map((guest, i) => {
+            return renderGuest(guest, i);
+          })}
+        </ul>
+        <ul className="m-2">
+          {exhibitors.map((guest, i) => {
+            return renderGuest(guest, i);
+          })}
+        </ul>
+        <ul className="m-2">
+          {artists.map((guest, i) => {
+            return renderGuest(guest, i);
+          })}
+        </ul>
+        <ul className="m-2">
+          {writers.map((guest, i) => {
+            return renderGuest(guest, i);
+          })}
+        </ul>
+      </SwipeableViews>
     </div>
   );
+
+  function renderGuest(guest: typeof guests[0], index: number) {
+    const isBookmarked = bookmarks?.includes(guest.slug);
+    return (
+      <li key={index}>
+        <Card className={index === 0 ? 'mt-1 mb-5' : 'my-5'}>
+          <button
+            className="w-full focus:outline-none"
+            onClick={handleClick(guest.website.link)}
+          >
+            <img className="w-full rounded" src={guest.img} alt={guest.slug} />
+          </button>
+          <div className="flex items-center justify-between">
+            <button
+              className="w-10/12 px-4 py-2 focus:outline-none"
+              onClick={handleClick(guest.website.link)}
+            >
+              <div className="text-2xl text-left">{guest.name}</div>
+              <div className="text-sm text-left">{guest.description}</div>
+            </button>
+            <div className="w-2/12 pr-3">
+              <button
+                className={`w-12 h-12 p-1 border-2 ${
+                  isBookmarked
+                    ? 'bg-green-500 border-green-600'
+                    : 'bg-white border-gray-400'
+                } focus:outline-none block mx-auto shadow-md`}
+                onClick={() => handleBookmark(guest.slug)}
+                style={{ borderRadius: 50 }}
+              >
+                <FontAwesomeIcon
+                  className={`text-2xl ${
+                    isBookmarked ? 'text-white' : 'text-green-400'
+                  } my-2`}
+                  icon="heart"
+                />
+              </button>
+            </div>
+          </div>
+        </Card>
+        {/* </button> */}
+      </li>
+    );
+  }
 };
 
 export default GuestList;
--- src/routes/Schedule.tsx
+++ src/routes/Schedule.tsx
@@ -11,8 +11,10 @@ import useBookmarks from '../hooks/useBookmarks';
 import firebaseApp from '../util/firebaseApp';
 import getFormattedEventTime from '../util/getFormattedEventTime';
 
+import SwipeableViews from 'react-swipeable-views';
+
 const DayButton = classed.button<{ current?: boolean }>`
-  w-1/3
+  w-2/5
   border
   rounded-lg
   border-green-600
@@ -21,13 +23,13 @@ const DayButton = classed.button<{ current?: boolean }>`
   mx-2
   font-bold
   ${({ current }) =>
-    current ? "bg-green-600 text-white" : "bg-white text-green-700"}
+    current ? 'bg-green-600 text-white' : 'bg-white text-green-700'}
 `;
 
 const Schedule: React.FC = () => {
   const { day_one: saturday, day_two: sunday } = eventSchedule.schedule_2020;
-  const [day, setDay] = React.useState<"sat" | "sun">("sat");
   const [showBookmark, setShowBookmark] = React.useState(false);
+  const [index, setIndex] = React.useState(0);
   const [user] = useAuthState(firebaseApp.auth());
   const [bookmarks, , , handleBookmark] = useBookmarks(user);
   const history = useHistory();
@@ -36,76 +38,97 @@ const Schedule: React.FC = () => {
     history.push(`/event/${slug}`);
   };
 
-  const handleDayToggle = (day: "sat" | "sun") => () => {
-    setDay(day);
+  const handleDayToggle = (index: number) => () => {
+    setIndex(index);
   };
 
   const handleBookmarkToggle = () => {
+    console.log('change');
     setShowBookmark(!showBookmark);
   };
 
-  let eventsToDisplay = day === "sat" ? saturday.events : sunday.events;
+  const handleIndexChange = (index: number, indexLatest: number) => {
+    setIndex(index);
+  };
+
+  let satEvents = saturday.events;
+  let sunEvents = sunday.events;
 
   if (showBookmark) {
-    eventsToDisplay = eventsToDisplay.filter(event =>
-      bookmarks?.includes(event.slug)
-    );
+    satEvents = satEvents.filter(event => bookmarks?.includes(event.slug));
+    sunEvents = sunEvents.filter(event => bookmarks?.includes(event.slug));
   }
 
   return (
     <div>
       <div className="flex items-center justify-between w-full my-2">
-        <div className="flex-grow">
-          <DayButton current={day === "sat"} onClick={handleDayToggle("sat")}>
+        <div className="flex-grow my-2">
+          <DayButton current={index === 0} onClick={handleDayToggle(0)}>
             Saturday
           </DayButton>
-          <DayButton current={day === "sun"} onClick={handleDayToggle("sun")}>
+          <DayButton current={index === 1} onClick={handleDayToggle(1)}>
             Sunday
           </DayButton>
         </div>
-        {bookmarks && (
-          <Toggle onChange={handleBookmarkToggle}>Show bookmarks</Toggle>
-        )}
+        {bookmarks && <Toggle onChange={handleBookmarkToggle} />}
       </div>
-      <ul>
-        {eventsToDisplay.map((event, i) => {
-          const isBookmarked = bookmarks?.includes(event.slug);
-          return (
-            <li key={i}>
-              <Card className="flex items-center justify-between my-4">
-                <button
-                  className="w-10/12 px-4 py-2"
-                  onClick={handleClick(event.slug)}
-                >
-                  <div className="mr-12 text-xl text-left">{event.name}</div>
-                  <div className="text-left">
-                    {getFormattedEventTime(event.begin, event.end)}
-                  </div>
-                </button>
-                <div className="w-2/12 pr-3">
-                  <button
-                    className={`w-12 h-12 p-1 border-2 ${
-                      isBookmarked
-                        ? "bg-green-500 border-green-600"
-                        : "bg-white border-gray-400"
-                    } focus:outline-none block mx-auto shadow-md rounded-full`}
-                    onClick={() => handleBookmark(event.slug)}
-                  >
-                    <FontAwesomeIcon
-                      className={`text-2xl ${
-                        isBookmarked ? "text-white" : "text-green-400"
-                      } my-2`}
-                      icon="heart"
-                    />
-                  </button>
-                </div>
-              </Card>
-            </li>
-          );
-        })}
-      </ul>
+      <SwipeableViews
+        index={index}
+        containerStyle={{
+          height: '100vh',
+          WebkitOverflowScrolling: 'touch',
+        }}
+        onChangeIndex={handleIndexChange}
+      >
+        <ul className="m-2">
+          {satEvents.map((event, i) => {
+            return renderEvent(event, i);
+          })}
+        </ul>
+        <ul className="m-2">
+          {sunEvents.map((event, i) => {
+            return renderEvent(event, i);
+          })}
+        </ul>
+      </SwipeableViews>
     </div>
   );
+
+  function renderEvent(event: typeof saturday.events[0], index: number) {
+    const isBookmarked = bookmarks?.includes(event.slug);
+    return (
+      <li key={index}>
+        <Card className="flex items-center justify-between my-4">
+          <button
+            className="w-10/12 px-4 py-2"
+            onClick={handleClick(event.slug)}
+          >
+            <div className="mr-12 text-xl text-left">{event.name}</div>
+            <div className="text-left">
+              {getFormattedEventTime(event.begin, event.end)}
+            </div>
+          </button>
+          <div className="w-2/12 pr-3">
+            <button
+              className={`w-12 h-12 p-1 border-2 ${
+                isBookmarked
+                  ? 'bg-green-500 border-green-600'
+                  : 'bg-white border-gray-400'
+              } focus:outline-none block mx-auto shadow-md rounded-full`}
+              onClick={() => handleBookmark(event.slug)}
+            >
+              <FontAwesomeIcon
+                className={`text-2xl ${
+                  isBookmarked ? 'text-white' : 'text-green-400'
+                } my-2`}
+                icon="heart"
+              />
+            </button>
+          </div>
+        </Card>
+      </li>
+    );
+  }
 };
 
 export default Schedule;
