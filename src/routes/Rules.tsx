54f5551d0210857dbe81a675687dc22187ed0dec
Sergio Nizama <sergio.nizama@outlook.com>
2020-02-08 23:17:09 -0500

    removed colon from headings

--- src/routes/Rules.tsx
+++ src/routes/Rules.tsx
@@ -9,7 +9,7 @@ const H3 = classed('h3')`
 `;
 
 const P = classed('p')`
-  mb-10
+  mb-8
 `;
 
 // const rules = [
@@ -80,19 +80,19 @@ const Rules: React.FC = () => {
         convention rooms, including in hotel room parties during and after
         SizeCon.
       </P>
-      <H3>PLEASE RESPECT BOUNDARIES OUTSIDE THE CON FLOOR:</H3>
+      <H3>PLEASE RESPECT BOUNDARIES OUTSIDE THE CON FLOOR</H3>
       <P>
         Outside of the convention doors, we ask our attendees to adhere to what
         is generally considered appropriate in terms of dress. Please refrain
         from bringing costumes or masks that may scare or intimidate guests.{' '}
       </P>
-      <H3>PLEASE RESPECT BOUNDARIES ON THE CON FLOOR:</H3>
+      <H3>PLEASE RESPECT BOUNDARIES ON THE CON FLOOR</H3>
       <P>
         Badges will be marked to distinguish whether given attendees are
         comfortable with adult content and discussions. Please be considerate of
         attendees unwilling to discuss in such a manner!
       </P>
-      <H3>NO PHOTOS OR VIDEOS ON THE CON FLOOR:</H3>
+      <H3>NO PHOTOS OR VIDEOS ON THE CON FLOOR</H3>
       <P>
         Attendees are asked to apply stickers to their phone cameras while
         enjoying the convention. We have an official photographer who is allowed
@@ -104,7 +104,7 @@ const Rules: React.FC = () => {
         doesn’t matter who or how we love, but what we love: our common size
         fascination is what’s bringing us all together.
       </P>
-      <H3>NO HARASSMENT:</H3>
+      <H3>NO HARASSMENT</H3>
       <P>
         We have a zero-tolerance policy for harassment, conducted by attendees
         and staff alike, which can include but is not limited to: unwelcome
@@ -115,7 +115,7 @@ const Rules: React.FC = () => {
         empowered to expel harassing parties from the convention and its related
         online spaces.
       </P>
-      <H3>PLEASE REPORT TO STAFF IF YOU FIND YOURSELF BEING HARASSED:</H3>
+      <H3>PLEASE REPORT TO STAFF IF YOU FIND YOURSELF BEING HARASSED</H3>
       <P>
         We’re interested in keeping our attendees safe and our atmosphere
         pleasant. If another attendee disrupts your experience enough to make
@@ -123,12 +123,12 @@ const Rules: React.FC = () => {
         report at any time, whether over email, in-person, via our Discord
         server, and so on.
       </P>
-      <H3>PLEASE COMPLY WITH STOP REQUESTS:</H3>
+      <H3>PLEASE COMPLY WITH STOP REQUESTS</H3>
       <P>
         If a member of staff asks you to stop doing something potentially
         disruptive, please stop doing it.
       </P>
-      <H3>WE VALUE YOUR TRUST:</H3>
+      <H3>WE VALUE YOUR TRUST</H3>
       <P>
         We advise our staff to refrain from making advances on attendees
         (intimate, romantic, or otherwise) while they are performing duties as
