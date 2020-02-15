3eb6a09af7b21f9de55bad7c6b90544154fbdc70
Sergio Nizama <sergio.nizama@outlook.com>
2020-02-15 12:18:29 -0500

    fixed firebaser

--- src/util/firebaseApp.ts
+++ src/util/firebaseApp.ts
@@ -3,14 +3,13 @@ import 'firebase/firestore';
 import firebase from 'firebase/app';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: 'AIzaSyAhpVlIqPc0kWI04HbU3kEfqWoWtyKHQ50',
-  authDomain: 'neo-post-reality-production.firebaseapp.com',
-  databaseURL: 'https://neo-post-reality-production.firebaseio.com',
-  projectId: 'neo-post-reality-production',
-  storageBucket: 'neo-post-reality-production.appspot.com',
-  messagingSenderId: '700110547611',
-  appId: '1:700110547611:web:9eabd6c7221abcbc53f43e',
-  measurementId: 'G-5D7VGL1H95',
+  apiKey: "AIzaSyAGNIex01Bc1hnKuXTMWPQ7zD-s5q8CDM0",
+  authDomain: "sizecon-app.firebaseapp.com",
+  databaseURL: "https://sizecon-app.firebaseio.com",
+  projectId: "sizecon-app",
+  storageBucket: "sizecon-app.appspot.com",
+  messagingSenderId: "131991271247",
+  appId: "1:131991271247:web:59f712a00312132743f73a"
 });
 
 export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
